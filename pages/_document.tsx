ac8dc10cd49f9305bb13255494ebdda9498489c7
Akhil Nair <4khiln4ir@gmail.com>
2020-09-17 11:46:43 +0530

    feat: SEO Improvements

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,12 +8,13 @@ export default class CustomDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta name="robots" content="all" />
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="keywords" content="Akhil Nair, Portfolio, Resume, AkhilxNair" />
           <meta name="author" content="Akhil Nair" />
           <meta name="description" content="Personal Portfolio page of Akhil Nair" />
-          <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+          <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
