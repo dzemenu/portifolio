ca405c2dc0e7908866f6fe3c5a6a8bd8934902d0
Akhil Nair <4khiln4ir@gmail.com>
2020-09-18 01:59:03 +0530

    feat: Added social links

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 // Import Modules
 import PropTypes from 'prop-types';
 import Router from 'next/router';
+import Head from 'next/head';
 
 // Import Styles
 import '../styles/globals.scss';
@@ -10,6 +11,18 @@ import '../styles/globals.scss';
 
 const MyApp = ({ Component, pageProps }) => (
   <>
+    <Head>
+      <meta name="robots" content="all" />
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="keywords" content="Akhil Nair, Portfolio, Resume, AkhilxNair" />
+      <meta name="author" content="Akhil Nair" />
+      <meta name="description" content="Personal Portfolio page of Akhil Nair" />
+      <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
+      <meta name="mobile-web-app-capable" content="yes" />
+    </Head>
     {/* <NavBar /> */}
     <Component {...pageProps} />
   </>
@@ -28,7 +41,7 @@ Router.events.on('routeChangeError', () => {
 });
 
 MyApp.propTypes = {
-  Component: PropTypes.element.isRequired,
+  Component: PropTypes.func.isRequired,
   pageProps: PropTypes.objectOf(PropTypes.object).isRequired,
 };
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,21 +8,12 @@ export default class CustomDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta name="robots" content="all" />
-          <meta charSet="utf-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="keywords" content="Akhil Nair, Portfolio, Resume, AkhilxNair" />
-          <meta name="author" content="Akhil Nair" />
-          <meta name="description" content="Personal Portfolio page of Akhil Nair" />
-          <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5" />
+          <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossOrigin="anonymous" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-          <meta name="msapplication-TileColor" content="#da532c" />
-          <meta name="theme-color" content="#ffffff" />
-          <meta name="mobile-web-app-capable" content="yes" />
         </Head>
         <body>
           <Main />
